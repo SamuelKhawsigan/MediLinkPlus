acc7d675322744550117f5d5a4841c3754830707
Samuel Khawsigan <samuelkhawsigan.it@gmail.com>
2025-10-23 18:38:31 +0800

    Initial Medilink JavaFX setup

---
+++ src/main/java/com/medilinkplus/MainApp.java
@@ -0,0 +1,23 @@
+package com.medilinkplus;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+    @Override
+    public void start(Stage stage) {
+        Label label = new Label("MediLink+ Setup Successful 🎉");
+        StackPane root = new StackPane(label);
+        Scene scene = new Scene(root, 400, 300);
+        stage.setScene(scene);
+        stage.setTitle("MediLink+ Test Window");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
